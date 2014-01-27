08eb2eb7712ac1c90d1f8b0d2f37871b495eea4a
Tomás Vilariño <vifito@vifito.eu>
2014-01-27 13:50:27 +0100

    Primeiro commit

---
+++ src/main/java/es/usc/supostos/App.java
@@ -0,0 +1,13 @@
+package es.usc.supostos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
